da347df0566619461df0def4713bea1ea5781ead
yuanweili <liyuanwei3@xiaomi.com>
2025-03-25 10:52:33 +0800

    pretrain llava

--- LLaVA/scripts/v1_5/pretrain.sh
+++ LLaVA/scripts/v1_5/pretrain.sh
@@ -21,7 +21,7 @@ deepspeed --include localhost:${gpus} --master_port ${port} ../../llava/train/tr
     --num_train_epochs 1 \
     --per_device_train_batch_size 32 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 2 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 24000 \
